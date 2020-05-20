35cb042a9a8b0801bfab92a3071934892a660704
Brendon Martin <brendonmartin657@gmail.com>
2020-05-20 14:10:28 -0400

    added onclicks to html. added functionality to app.js.

---
+++ app.js
@@ -0,0 +1,88 @@
+let num1 = { value: '0' };
+let num2 = { value: null };
+let hasOperation = false;
+let operator = null;
+
+//Display and Number Functions
+function updateDisplay() {
+	let display = document.getElementById('display');
+
+	if (!hasOperation) {
+		display.innerHTML = `${num1.value}`;
+	} else if (num2.value === null) {
+		display.innerText = `${num1.value} ${operator}`;
+	} else {
+		display.innerText = `${num1.value} ${operator} ${num2.value}`;
+	}
+}
+function findNumberToAppend() {
+	if (!hasOperation) {
+		return num1;
+	} else {
+		return num2;
+	}
+}
+function numberSelect(num) {
+	let number = findNumberToAppend();
+	if (number.value === '0' && num === '0') {
+		return;
+	} else if ((number.value === '0' || number.value === null) && num !== '0') {
+		number.value = num;
+	} else {
+		number.value += num;
+	}
+	updateDisplay();
+}
+
+//Operation and Equal Functions
+function operationSelect(operation) {
+	operator = operation;
+	hasOperation = true;
+	updateDisplay();
+}
+function equalSelect() {
+	let number1 = Number(num1.value);
+	let number2 = Number(num2.value);
+
+	switch (operator) {
+		case '+':
+			num1.value = String(number1 + number2);
+			break;
+		case '-':
+			num1.value = String(number1 - number2);
+			break;
+		case '*':
+			num1.value = String(number1 * number2);
+			break;
+		case '/':
+			num1.value = String(number1 / number2);
+			break;
+	}
+
+	resetAfterEqual();
+	updateDisplay();
+}
+
+//Clear and Reset Functions
+function resetAfterEqual() {
+	operator = null;
+	hasOperation = false;
+	num2.value = null;
+}
+function clearSelect() {
+	num1.value = '0';
+	num2.value = null;
+	hasOperation = false;
+	operator = null;
+	updateDisplay();
+}
+function clearEntrySelect() {
+	let number = findNumberToAppend();
+	number.value = '0';
+	updateDisplay();
+}
+function backspaceSelect() {}
+
+//Decimal and Plus Minus
+function decimalSelect() {}
+function plusMinusSelect() {}
